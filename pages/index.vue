048ad70e7fcdec6a0c73279c8a81b594d4b59d8a
79316758+koudashinya@users.noreply.github.com <79316758+koudashinya@users.noreply.github.com>
2022-02-06 16:18:55 +0900

    index.vue修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,57 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+ <div class="page">
+   <label>
+     <span>
+       お名前:
+     </span>
+     <input
+       type="text"
+       v-model="user.name"
+     >
+   </label>
+   <label>
+     <span>
+       email:
+     </span>
+     <input
+       type="text"
+       v-model="user.email"
+     >
+   </label>
+   <button
+     type="button"
+     @click="submit"
+   >
+     Submit
+   </button>
+ </div>
 </template>
 
 <script>
+import firebase from '@/plugins/firebase'
+
 export default {
-  name: 'IndexPage'
+ data () {
+   return {
+     user: {
+       name: "",
+       email: ""
+     },
+   }
+ },
+ methods: {
+   submit () {
+     const db = firebase.firestore()
+     let dbUsers = db.collection('users')
+     dbUsers
+       .add({
+         name: this.user.name,
+         email: this.user.email,
+       })
+       .then(ref => {
+         console.log('Add ID: ', ref.id)
+       })
+   },
+ },
 }
 </script>
