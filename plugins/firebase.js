dffb1d76e226d77148450886da99f4ab475039dd
79316758+koudashinya@users.noreply.github.com <79316758+koudashinya@users.noreply.github.com>
2022-02-06 17:57:07 +0900

    firebase.js修正

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,7 +1,10 @@
 import * as firebase from 'firebase/app'
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/functions";
 
-if (!firebase.apps.length) {
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyDezFVBBiixT2wG-druHfrp1VimapV7SRs",
   authDomain: "sample-c4f7c.firebaseapp.com",
   projectId: "sample-c4f7c",
@@ -9,7 +12,8 @@ firebase.initializeApp({
   messagingSenderId: "289920579051",
   appId: "1:289920579051:web:c9420fe4667bc8629195ae",
   measurementId: "G-1HZKTX0N0G"
-})
 }
+
+firebase.initializeApp(firebaseConfig);
 
 export default firebase
