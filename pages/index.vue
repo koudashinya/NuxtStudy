14fbfabcfdbcbd935e73a7bb5f5c675ade2d80fc
79316758+koudashinya@users.noreply.github.com <79316758+koudashinya@users.noreply.github.com>
2022-02-19 00:12:44 +0900

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,72 @@
 <template>
- <div class="page">
-   <label>
-     <span>
-       お名前:
-     </span>
-     <input
-       type="text"
-       v-model="user.name"
-     >
-   </label>
-   <label>
-     <span>
-       email:
-     </span>
-     <input
-       type="text"
-       v-model="user.email"
-     >
-   </label>
-   <button type="button" @click="submit">
-     Submit
-   </button>
-   <br>
-   <br>
-   <!--課題1-->
-   <!--v-modelでdataと連動させる-->
-   <input type="text" v-model="listAdd">
-   <button type="button" v-on:click="addTodo">追加ボタン</button>
-   <ul>
-     <li v-for="value in todos" :key="value.listId">
-       {{value.name}}
-     </li>
-   </ul>
+<div class="page">
+  <label>
+    <span>
+      お名前:
+    </span>
+    <input
+      type="text"
+      v-model="user.name"
+    >
+  </label>
+  <label>
+    <span>
+      email:
+    </span>
+    <input
+      type="text"
+      v-model="user.email"
+    >
+  </label>
+  <button type="button" @click="submit">
+    Submit
+  </button>
+  <br>
+  <br>
+  <!--課題1-->
+  <!--v-modelでdataと連動させる-->
+  <input type="text" v-model="listAdd">
+  <button type="button" @click="addTodo">追加ボタン</button>
+  <ul>
+    <li v-for="(value, index) in todos" :key="value.listId">
+      {{value.name}}
+    </li>
+  </ul>
   </div>
 </template>
 
 <script>
 
 export default {
- data () {
-   return {
-     user: {
-       name: "",
-       email: ""
-     },
-     addTodo:"",
-     todos: [],
-     num: 0,
-     // v-modelから渡ってくる値
-     listAdd: '',
-
-   }
- },
+  data () {
+    return {
+      user: {
+        name: "",
+        email: ""
+      },
+      todos: [],
+    // v-modelから渡ってくる値
+      listAdd: '',
+  }
+},
  methods: {
-   submit () {
-     const dbUsers = this.$firestore.collection('users')
-     dbUsers
-       .add({
-         name: this.user.name,
-         email: this.user.email,
-       })
-       .then(ref => {
-         console.log('Add ID: ', ref.id)
-       })
-   },
- },
- addTodo: function() {
-   this.todos.push({listId: this.num, name : 'listAdd'});
- }
- 
+    submit () {
+      const dbUsers = this.$firestore.collection('users')
+      dbUsers
+        .add({
+          name: this.user.name,
+          email: this.user.email,
+        })
+        .then(ref => {
+          console.log('Add ID: ', ref.id)
+        })
+    },
+    addTodo() {
+      this.todos.push({'name' : this.listAdd});
+    },
+    deleteTodo(index) {
+      this.listAdd;
+    }
+  },
 }
 </script>
