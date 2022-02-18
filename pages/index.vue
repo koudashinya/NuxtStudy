a6d9734a9364fa529b90b3fa89a1871d891d82e7
79316758+koudashinya@users.noreply.github.com <79316758+koudashinya@users.noreply.github.com>
2022-02-19 00:15:11 +0900

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -25,11 +25,14 @@
   <br>
   <!--課題1-->
   <!--v-modelでdataと連動させる-->
-  <input type="text" v-model="listAdd">
-  <button type="button" @click="addTodo">追加ボタン</button>
+  <h2>TODOLIST</h2>
+  <input type="text" v-model="newItem" >
+  <button @click="addItem">ADD</button>
   <ul>
-    <li v-for="(value, index) in todos" :key="value.listId">
-      {{value.name}}
+    <li v-for="(todo, index) in todos" :key="index">
+      <span v-bind:class="{activ: todo.isDone}">{{ todo.item }}</span>
+      <button @click="deleteItem(index)">削除</button>
+      <button @click="addLine(index)">完了</button>
     </li>
   </ul>
   </div>
@@ -45,28 +48,57 @@ export default {
         email: ""
       },
       todos: [],
-    // v-modelから渡ってくる値
-      listAdd: '',
+      // v-modelから渡ってくる値
+      newItem: ""
   }
 },
- methods: {
-    submit () {
-      const dbUsers = this.$firestore.collection('users')
-      dbUsers
-        .add({
-          name: this.user.name,
-          email: this.user.email,
-        })
-        .then(ref => {
-          console.log('Add ID: ', ref.id)
-        })
-    },
-    addTodo() {
-      this.todos.push({'name' : this.listAdd});
-    },
-    deleteTodo(index) {
-      this.listAdd;
-    }
-  },
+  methods: {
+    submit() {
+        const dbUsers = this.$firestore.collection('users')
+        dbUsers
+          .add({
+            name: this.user.name,
+            email: this.user.email,
+          })
+          .then(ref => {
+            console.log('Add ID: ', ref.id)
+          })
+      },
+// -------------課題------------------------------
+      addItem: function(event) {
+        // オブジェクト作成
+        const todo = {
+          item: this.newItem,
+          isDone: false
+        }
+        // 入力値をオブジェクトごと配列に入れる
+        this.todos.push(todo)
+        // textboxリセット処理
+        this.newItem = ""
+        // 空文字入力はスルー
+        // if(this.newItem === "") return
+        // todoオブジェクトを作成して配列で出力できるように
+        // let todo = {
+        //   item: this.newItem
+        // }
+      },
+      // 削除処理
+      deleteItem: function(index) {
+        this.todos.splice(index, 1)
+      },
+      // 配列index番目の条件を反転する
+      addLine(index) {
+        this.$set(this.todos[index], 'isDone', !this.todos[index].isDone)
+      }
+  }
 }
 </script>
+
+<style scoped>
+#app ul{
+  list-style:none;
+}
+.activ {
+  text-decoration: line-through;
+}
+</style>
